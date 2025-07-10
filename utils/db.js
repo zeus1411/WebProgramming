3d5753bcb53132a9fa20645e04b09a6db322397b
Zeus1411 <121225888+zeus1411@users.noreply.github.com>
2025-07-10 11:55:05 +0700

    install some package

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const knex = knexObj({
   client: 'mysql2',
   connection: {
     host: '127.0.0.1', //host: 'localhost',
-    port: 3306,
+    port: 3307,
     user: 'root',
     password: '',
     database: 'webupdate'
